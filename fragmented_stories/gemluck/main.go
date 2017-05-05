59b344976cd08b39859f4aead659d6c8929f6a3f
Justine Lera <justinelera@gmail.com>
2017-05-05 16:34:07 +0200

    a way to evaluate your worth

---
+++ fragmented_stories/gemluck/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func random(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
+}
+
+func main() {
+	fmt.Println()
+
+	gemstash := random(1, 20)
+
+	switch {
+	case gemstash < 10:
+		fmt.Printf("Looking for gems in your clothing.\n")
+		fmt.Println("You find", gemstash)
+	case gemstash >= 10:
+		fmt.Println("In your pocket, you find", gemstash, "gems.")
+		fmt.Println("Two gems to rub together. Better than yesterday.")
+	}
+	fmt.Println()
+}
