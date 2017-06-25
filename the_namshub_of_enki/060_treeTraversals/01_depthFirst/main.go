a68061995be2de85a97880dd6a690255352a8f93
Justine Lera <justinelera@gmail.com>
2017-06-25 18:54:41 +0200

    added explanations of types of traversal

--- the_namshub_of_enki/060_treeTraversals/01_depthFirst/main.go
+++ the_namshub_of_enki/060_treeTraversals/01_depthFirst/main.go
@@ -2,16 +2,7 @@
 
 /*
 Applicable only to binary trees.
-° Start at the top.
-° If there is a new left child, check for and name children
-° If there are no new children, return to previous level.
-° If there is a new right child, check for and name children
 
-... this is root, leftChild, rightChild
-
-This can be simplified:
-Q1: If there is a new left child, go to the child, and ask this again, otherwise go to Q2.
-Q2: If there is a new right child, go to the child and ask Q1, otherwise go up a level.
 
 */
 
--- the_namshub_of_enki/060_treeTraversals/04_post-order/main.go
+++ the_namshub_of_enki/060_treeTraversals/04_post-order/main.go
@@ -1,3 +1,10 @@
+
+// Post-order Traversal
+
+/*
+
+
+
 package main
 
 import "fmt"
